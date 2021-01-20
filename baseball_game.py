e87bd9812a0cf065db2e44a9b91523d66482842b
kyungmin1212 <uiuiui12@naver.com>
2021-01-20 15:41:59 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -277,22 +277,29 @@ def is_no(one_more_input):
 
 def main():
     print("Play Baseball")
-    user_input = input('Input guess number : ')
-    if not is_digit(user_input) or is_duplicated_number(user_input):
-        print('Wrong Input')
-        return
-    
     random_number = str(get_not_duplicated_three_digit_number())
     print("Random Number is : ", random_number)
+    user_input="999"
+
+    while user_input!=random_number:
+        while user_input!="0":
+            user_input = input('Input guess number : ')
+            if user_input=="0":
+                break
+            elif not is_digit(user_input) or is_duplicated_number(user_input):
+                print('Wrong Input, Input again')
+            else:
+                break
+        if user_input=="0":
+            break
+        print(f"Strikes : {get_strikes_or_ball(user_input, random_number)[0]}, Balls : {get_strikes_or_ball(user_input, random_number)[1]}")
     if get_strikes_or_ball(user_input, random_number)==[3,0]:
-        print("You win, one more(Y/N) ?\n")
+        print("You win, one more(Y/N) ?",end="")
         check=input()
         if is_yes(check):
             main()
         elif is_no(check):
             pass
-    else:
-        main()
     # ===Modify codes below=============
     # 위의 코드를 포함하여 자유로운 수정이 가능함
 
